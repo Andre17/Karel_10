92ee7e00157e10d356d225a8864d14362f0f37f6
Andre17 <andrepurits@Andre-Puritss-MacBook-Pro-3.local>
2014-12-10 12:06:13 +0000

    Lab 10

--- Lab10.java
+++ Lab10.java
@@ -1,23 +1,42 @@
 /**
  * An algorithm helps an Athlete solve a maze, perhaps by keeping one hand against a wall.
  * 
- * @author <...>
+ * @author <Andre Purits>
  * @version <...>
  *
  */
 
 import edu.fcps.karel2.Display; 
+import javax.swing.JOptionPane;
 
 public class Lab10 {
 	 
+    public static void clearMaze(Athlete arg) {
+		 //TODO Write an algorithm that directs arg to the beeper at the end of the maze
+       
+       if(arg.rightIsClear()) {
+       arg.turnRight();
+       arg.move();
+       } else if(arg.frontIsClear()) {
+       arg.move();
+       } else if(arg.leftIsClear()) {
+       arg.turnLeft();
+       arg.move();
+       } else {
+       arg.turnAround();
+       arg.move();
+       }
+    }   
 	 public static void main(String[] args) {
 		 //TODO Open the map 'maze1' or 'maze2' or 'maze3';
 		 //TODO Set the display to size 10 by 10
+       String map = JOptionPane.showInputDialog("Which maze?");
+		 Display.openWorld("maps/"+map+".map");
+		 Display.setSize(10, 10);
+       Display.setSpeed(10);
 		 Athlete athena = new Athlete(1, 1, Display.NORTH, Display.INFINITY);
-		 clearMaze(athena);
+		 while(!athena.nextToABeeper()) {
+         clearMaze(athena);
+       }
 	 }
-	 
-	 public static void clearMaze(Athlete arg) {
-		 //TODO Write an algorithm that directs arg to the beeper at the end of the maze
-	 }
- }
+}
